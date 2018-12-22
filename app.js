e217c703089e35bb0a384536b1e434598bafd7a2
Sujit Kumar Singh <sujit@soroco.com>
2018-12-23 00:34:57 +0530

    Adding Handlebars templates

--- app.js
+++ app.js
@@ -3,24 +3,30 @@ const rootDir = require('./utils/paths');
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
-const server = express();
-const adminRoutes = require('./routes/admin');
+const app = express();
+
+app.engine('hbs', expressHbs()); // defining handlebars engine
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+// importing routes
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-server.use(bodyParser.urlencoded({ extended: false }));
-server.use(express.static(path.join(rootDir, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, 'public')));
 
-// routes
-server.use('/admin', adminRoutes);
-server.use(shopRoutes);
-
+// using routes
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 // 404 route
-server.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: '404' });
 });
 
-const appServer = http.createServer(server);
+const appServer = http.createServer(app);
 appServer.listen(3000, ()=> {
   console.log('Listening');
 });
--- routes/shop.js
+++ routes/shop.js
@@ -3,8 +3,14 @@ const rootDir = require('../utils/paths');
 const express = require('express');
 const router = express.Router();
 
+const adminData = require('./admin');
+
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop', {
+    pageTitle: 'Shop',
+    prods: adminData.products,
+    hasProds: adminData.products.length > 0
+  });
 });
 
 module.exports = router;
