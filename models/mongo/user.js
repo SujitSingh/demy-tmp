cbd0c14436ebf6a5257f406e38e98576ac0bd325
Sujit Kumar Singh <sujit98322@gmail.com>
2020-01-05 23:01:55 +0530

    Adding Add, Get, and Delete Cart features using Mongoose

--- models/mongo/user.js
+++ models/mongo/user.js
@@ -17,4 +17,43 @@ const userSchema = mongoose.Schema({
   }
 });
 
+userSchema.methods.addToCart = function(product) {
+  const cartProductIndex = this.cart.items.findIndex(item => {
+    return item.productId.toString() === product._id.toString();
+  });
+  const updatedCartItems = [...this.cart.items || []];
+  if (cartProductIndex === -1) {
+    // add new cart item
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: 1
+    });
+  } else {
+    // increment existing item quantity
+    updatedCartItems[cartProductIndex].quantity++;
+  }
+  const updatedCart = {
+    items: updatedCartItems
+  };
+  this.cart = updatedCart;
+  return this.save(); // save changes
+}
+
+userSchema.methods.getCartItems = function() {
+  // fetch user details while populating on productId
+  return this.populate('cart.items.productId').execPopulate().then(userDetails => {
+    userDetails = userDetails ? userDetails.toJSON() : userDetails;
+    const cartItems = userDetails && userDetails.cart && userDetails.cart.items || [];
+    return cartItems;
+  });
+}
+
+userSchema.methods.deleteCartItem = function(productId) {
+  const updatedCartItems = this.cart.items.filter(item => {
+    return item.productId.toString() === productId.toString();
+  });
+  this.cart.items = updatedCartItems;
+  return this.save();
+}
+
 module.exports = mongoose.model('User', userSchema, 'Users');
