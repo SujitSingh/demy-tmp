06ec12a333891d6cd06d4f59a4c230cf549ea490
Sujit Kumar Singh <sujit98322@gmail.com>
2020-05-26 20:34:27 +0530

    Updating environment file for Nodemon to hold variables

--- utils/config.js
+++ utils/config.js
@@ -4,12 +4,12 @@ const demyConfig = {
   mongoDBPath: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB_NAME}`,
   serverPort: process.env.SERVER_PORT || 3300,
   emailerConfig: {
-    host: 'smtp.mailtrap.io',
+    host: process.env.SMPT_HOST,
     port: 2525,
-    user: '<username>',
-    pass: '<password>'
+    user: process.env.SMPT_USER,
+    pass: process.env.SMTP_PASS
   },
-  stripe: process.env.STRIPE_KEY || '<stripe-secret-token>',
+  stripe: process.env.STRIPE_KEY,
   productImgsRoot: 'public/imgs',
   invoiceFilesRoot: 'data/invoice'
 };
