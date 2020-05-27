8e3e2cf7229ac65a8245b5456418af409152dd5b
Sujit Kumar Singh <sujit98322@gmail.com>
2020-05-27 14:28:37 +0530

    Correction for Environment variable name

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,7 @@ const demyConfig = {
   useMongoDB: true,
   sessionSecret: process.env.SESSION_SECRET || 'some-session-secret',
   mongoDBPath: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB_NAME}`,
-  serverPort: process.env.SERVER_PORT || 3300,
+  serverPort: process.env.PORT || 3300,
   emailerConfig: {
     host: process.env.SMPT_HOST,
     port: 2525,
